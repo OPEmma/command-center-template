66e5d12747cc554ac5b3d60d50221a19ad65e574
OPEmma <your_whitejialan@gmail.com>
2026-06-20 14:49:44 +0100

    fix keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,12 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "hidden";
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "hidden";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://umiauevfaxqlfbuujskl.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtaWF1ZXZmYXhxbGZidXVqc2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NTA2NjYsImV4cCI6MjA5NzIyNjY2Nn0.PQ2pGl3UFBIbMYJn0rhX6d9S_fbhEZluvrYG3iiY6I0";
+
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 
 
-https://umiauevfaxqlfbuujskl.supabase.co
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtaWF1ZXZmYXhxbGZidXVqc2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NTA2NjYsImV4cCI6MjA5NzIyNjY2Nn0.PQ2pGl3UFBIbMYJn0rhX6d9S_fbhEZluvrYG3iiY6I0";
+
