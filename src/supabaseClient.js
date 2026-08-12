7093cbec6568c25a068a246c6ccf33cf1b6425d6
OPEmma <ed9317153@gmail.com>
2026-08-12 17:30:07 +0100

    add supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,11 +2,11 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
   (typeof window !== "undefined" && window.__ENV__?.VITE_SUPABASE_URL) ||
-  import.meta.env.VITE_SUPABASE_URL;
+  "https://umiauevfaxqlfbuujskl.supabase.co";
 
 const supabaseAnonKey =
   (typeof window !== "undefined" && window.__ENV__?.VITE_SUPABASE_ANON_KEY) ||
-  import.meta.env.VITE_SUPABASE_ANON_KEY;
+  import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtaWF1ZXZmYXhxbGZidXVqc2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NTA2NjYsImV4cCI6MjA5NzIyNjY2Nn0.PQ2pGl3UFBIbMYJn0rhX6d9S_fbhEZluvrYG3iiY6I0;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
